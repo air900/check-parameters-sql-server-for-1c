efab65428bf1fa1b1b62f54d79a20da4dd10a2c8
air900 <air900@github.com>
2026-03-14 12:57:21 +0100

    v1.0.3: fix install.ps1 encoding

--- scripts/powershell/Invoke-1CDiagnostic.ps1
+++ scripts/powershell/Invoke-1CDiagnostic.ps1
@@ -99,7 +99,7 @@ foreach ($mod in $requiredModules) {
 
 Write-Host ""
 Write-Host "  ================================================================" -ForegroundColor Cyan
-Write-Host "      ДИАГНОСТИКА PostgreSQL ДЛЯ 1С:ПРЕДПРИЯТИЕ  v1.0" -ForegroundColor Cyan
+Write-Host "      ДИАГНОСТИКА PostgreSQL ДЛЯ 1С:ПРЕДПРИЯТИЕ  v1.0.3" -ForegroundColor Cyan
 Write-Host "                      audit-reshenie.ru" -ForegroundColor Cyan
 Write-Host "  ================================================================" -ForegroundColor Cyan
 Write-Host ""
